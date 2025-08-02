be2f55fef6944b0074dae3bd0cabc73b7b7fb7a1
suixin1314 <56030771+suixin1314@users.noreply.github.com>
2025-08-02 17:50:27 +0800

    Update ipq60xx.mk
    Signed-off-by: suixin1314 <56030771+suixin1314@users.noreply.github.com>

--- target/linux/qualcommax/image/ipq60xx.mk
+++ target/linux/qualcommax/image/ipq60xx.mk
@@ -68,6 +68,19 @@ define Device/glinet_gl-ax1800
 endef
 TARGET_DEVICES += glinet_gl-ax1800
 
+define Device/zn_m2
+	$(call Device/FitImage)
+	$(call Device/UbiFit)
+	DEVICE_VENDOR := ZN
+	DEVICE_MODEL := M2
+	BLOCKSIZE := 128k
+	PAGESIZE := 2048
+	DEVICE_DTS_CONFIG := config@cp03-c1
+	SOC := ipq6018
+	DEVICE_PACKAGES := ipq-wifi-zn_m2
+endef
+TARGET_DEVICES += zn_m2
+
 define Device/glinet_gl-axt1800
 	$(call Device/glinet_gl-common)
 	DEVICE_MODEL := GL-AXT1800
